75a7955e35bcf7e047ef91f6c14bdc88185baa88
kitakuma <111678061+kitakuma@users.noreply.github.com>
2022-08-21 18:27:31 +0900

    Update aframe-gif-shader.js

--- dist/aframe-gif-shader.js
+++ dist/aframe-gif-shader.js
@@ -479,8 +479,8 @@
 	   * @public
 	   */
 	  nextFrame: function nextFrame() {
-            this.__fillCanvas();
-	    this.__draw();
+            this.__clearCanvas();
+	    /*this.__draw();*/
 
 	    /* update next frame time */
 	    while (Date.now() - this.__startTime >= this.__nextFrameTime) {
@@ -497,16 +497,7 @@
 	  /*==============================
 	   =            canvas            =
 	   ==============================*/
-
-		/**fill canvas**/
-		__fillCanvas: function __fillCanvas() {
-			    //塗りスタイルに青色を指定する
-    this.__ctx.fillStyle = "rgb(0, 0, 255)";
-    //左から20上から20の位置に幅50高さ50の塗りつぶしの四角形を描く
-    this.__ctx.fillRect(0,0,this.__width,this.__height);
-		}
-		
-	  /**
+          /**
 	   * clear canvas
 	   * @private
 	   */
